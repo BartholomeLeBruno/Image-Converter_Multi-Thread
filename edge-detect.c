943a7d95f3497ec186161a2574dbb21da6dbadf4
Daxxxan <benard_a141@myges.fr>
2020-01-15 23:59:18 +0100

    :fire: handle every parameters :fire:

--- edge-detect.c
+++ edge-detect.c
@@ -12,31 +12,40 @@
 #define LENGHT DIM
 #define OFFSET (DIM / 2)
 
-const float KERNEL[DIM][DIM] = {{-1, -1, -1},
-								{-1, 8, -1},
-								{-1, -1, -1}};
+const float EDGE_DETECT[DIM][DIM] = {{-1, -1, -1},
+								    {-1, 8, -1},
+								    {-1, -1, -1}};
+
+const float BOX_BLUR[DIM][DIM] = {{0.11,0.11,0.11},
+                                  {0.11,0.11,0.11},
+                                  {0.11,0.11,0.11}};
+
+const float SHARPEN[DIM][DIM] = {{0, -1,0},
+                                 {-1,5,-1},
+                                 {0,-1,0}};
 
 typedef struct stack_t {
     char** path;
     int count;
     int max;
+    char* effect;
 } Stack;
 
 static pthread_mutex_t mutex;
 static pthread_cond_t is_img_ok;
 static Stack stack;
-
 struct Img stackImg[50000];
-struct Img openImage(char *path);
 
+struct Img openImage(char *path);
 void *producer(void *img);
 void *consumer();
 void *apply_effect(void *img);
+void launchThreads(int numberThreads, pthread_attr_t attr, pthread_t threads[]);
+void fillStackImage();
+void verifyNumberThreadsIsValid(int numberThreads);
 
 
-void *producer(void *img)
-{
-    printf("producer: stack.count: %d\n", stack.max);
+void *producer(void *img) {
     pthread_mutex_lock(&mutex);
 
     char* imgPath = (char *)img;
@@ -55,9 +64,7 @@ void *producer(void *img)
 	return NULL;
 }
 
-void *consumer()
-{
-    printf("stack: %d\n", stack.max);
+void *consumer() {
 	while(1) {
         pthread_mutex_lock(&mutex);
         while(isempty()) {
@@ -77,14 +84,13 @@ void *consumer()
 	return NULL;
 }
 
-struct Img openImage(char *path)
-{
+struct Img openImage(char *path) {
 	struct Img Img;
 	Img.original = open_bitmap(path);
 	return Img;
 }
 
-void fillStackImage() {
+void fillStackImage(char* originFolder) {
     DIR *d;
     struct dirent *dir;
     int counter = 0;
@@ -92,7 +98,7 @@ void fillStackImage() {
     stack.path = (char *)malloc(sizeof(char*));
     stack.count = 0;
 
-    d = opendir("image");
+    d = opendir(originFolder);
     while ((dir = readdir(d)) != NULL)
     {
         if (strstr(dir->d_name, ".bmp") != NULL)
@@ -109,22 +115,8 @@ void fillStackImage() {
     stack.max = stack.count;
 }
 
-int main(int argc, char **argv)
-{
-	int counter = 0;
-	int numberThreads = 3;
-    char imagePath[numberThreads][255];
-
-    pthread_t threads[numberThreads];
-	pthread_attr_t attr;
-	pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-
-	pthread_cond_init(&is_img_ok, NULL);
-	pthread_mutex_init(&mutex, NULL);
-
-	fillStackImage();
-	printf("stack.count: %d\n", stack.max);
+void launchThreads(int numberThreads, pthread_attr_t attr, pthread_t threads[]) {
+    int counter = 0;
 
     while(stack.count > 0) {
         pthread_create(&threads[counter], &attr, producer, stack.path[stack.count - 1]);
@@ -136,16 +128,77 @@ int main(int argc, char **argv)
             counter = 0;
         }
     }
+}
 
-	pthread_create(&threads[1], NULL, consumer, NULL);
-	pthread_join(threads[1], NULL);
+void verifyNumberThreadsIsValid(int numberThreads) {
+    if(numberThreads <= 0 || numberThreads > stack.count) {
+        printf("Number of threads is incorrect\n");
+        exit(1);
+    }
+}
+
+void verifyFolderIsValid(char* folder) {
+    if(!folder) {
+        printf("One folder is missing\n");
+        exit(1);
+    }
+}
+
+void verifyDestinationFolderIsValid(char* destinationFolder) {
+    DIR *d;
+    struct dirent *dir;
+    char fileName[255] = "";
+
+    verifyFolderIsValid(destinationFolder);
+
+    d = opendir(destinationFolder);
+    while ((dir = readdir(d)) != NULL) {
+        strcpy(fileName, "");
+        strcat(strcat(fileName, destinationFolder), dir->d_name);
+        unlink(fileName);
+    }
+}
+
+void affectEffect(char* effect) {
+    stack.effect = malloc(strlen(effect) * sizeof(char));
+    stack.effect = effect;
+}
+
+void verifyAllParametersAreGiven(char **argv) {
+    if(!argv[1] || !argv[2] || !argv[3] || !argv[4]) {
+        printf("Veuillez entrer tous les arguments: ./apply-effect \"./in/\" \"./out/\" 3 BOX_BLUR\n");
+        exit(1);
+    }
+}
+
+int main(int argc, char **argv) {
+    verifyAllParametersAreGiven(argv);
+    verifyFolderIsValid(argv[1]);
+    verifyDestinationFolderIsValid(argv[2]);
+	int numberThreads = atoi(argv[3]);
+	affectEffect(argv[4]);
+
+    fillStackImage(argv[1]);
+    verifyNumberThreadsIsValid(numberThreads);
+
+    pthread_t threads[numberThreads];
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+
+    pthread_cond_init(&is_img_ok, NULL);
+    pthread_mutex_init(&mutex, NULL);
+
+    launchThreads(numberThreads, attr, threads);
+
+	pthread_create(&threads[numberThreads - 1], NULL, consumer, NULL);
+	pthread_join(threads[numberThreads -1], NULL);
 	pthread_cond_destroy(&is_img_ok);
 	pthread_mutex_destroy(&mutex);
 	return 0;
 }
 
-void *apply_effect(void *img)
-{
+void *apply_effect(void *img) {
 
     int w = ((struct Img *)img)->original.bmp_header.width;
     int h = ((struct Img *)img)->original.bmp_header.height;
@@ -166,10 +219,22 @@ void *apply_effect(void *img)
                     int yn = y + b - OFFSET;
 
                     Pixel *p = &(((struct Img *)img)->original).pixel_data[yn][xn];
-
-                    c.Red += ((float)p->r) * KERNEL[a][b];
-                    c.Green += ((float)p->g) * KERNEL[a][b];
-                    c.Blue += ((float)p->b) * KERNEL[a][b];
+                    if(strcmp(stack.effect, "EDGE_DETECT") == 0) {
+                        c.Red += ((float) p->r) * EDGE_DETECT[a][b];
+                        c.Green += ((float) p->g) * EDGE_DETECT[a][b];
+                        c.Blue += ((float) p->b) * EDGE_DETECT[a][b];
+                    } else if(strcmp(stack.effect, "BOX_BLUR") == 0) {
+                        c.Red += ((float) p->r) * BOX_BLUR[a][b];
+                        c.Green += ((float) p->g) * BOX_BLUR[a][b];
+                        c.Blue += ((float) p->b) * BOX_BLUR[a][b];
+                    } else if(strcmp(stack.effect, "SHARPEN") == 0) {
+                        c.Red += ((float) p->r) * SHARPEN[a][b];
+                        c.Green += ((float) p->g) * SHARPEN[a][b];
+                        c.Blue += ((float) p->b) * SHARPEN[a][b];
+                    } else {
+                        printf("Le type d'effet n'est pas bon\n");
+                        exit(1);
+                    }
                 }
             }
 
